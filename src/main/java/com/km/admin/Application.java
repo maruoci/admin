0d0cce8a24345af62fa2583e587e3ba5d30faecf
maruoci <makun_11@126.com>
2018-06-28 21:07:54 +0800

    init

--- src/main/java/com/km/admin/Application.java
+++ src/main/java/com/km/admin/Application.java
@@ -2,14 +2,26 @@ package com.km.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 入口应用
  */
 @SpringBootApplication
+@Controller
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  /**
+   * test
+   * @return
+   */
+  @RequestMapping("/hello")
+  public String hello(){
+    return "index";
   }
 }
