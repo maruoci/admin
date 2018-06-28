be0c0c00df5994364f59558d3c4ca2aadadac60e
maruoci <makun_11@126.com>
2018-06-28 18:09:14 +0800

    init01

---
+++ src/main/java/com/km/admin/Application.java
@@ -0,0 +1,15 @@
+package com.km.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 入口应用
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
